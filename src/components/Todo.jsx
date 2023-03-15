03df29d5ab85667e3eb5cd897efddce22d70f360
chiomavera <you@example.com>
2023-03-15 15:40:24 +0100

    [CHORE]: nest icons inside buttons nested the the trash and edit icons insided button in order to allow for focus when tabbing throug the page this is to ensure accessbility of the page

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,29 +1,42 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenSquare } from "@fortawesome/free-solid-svg-icons";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
-
-const Todo = ({task, onCheck, deleteTodo, editTodo}) => {
+const Todo = ({ task, onCheck, deleteTodo, editTodo }) => {
   return (
     <>
       <div className="Todo">
-      
-      <div style={{ textDecoration: task.completed ? 'line-through' : 'none',
-       textDecorationColor: task.completed ? "#eee" : "inherit" }}>
-      <input
-        type="checkbox"
-        checked={task.completed}
-        onChange={() => onCheck(task.id)}
-        className="check__box"
-      />
-      {task.task}
-    </div>
+        <div
+          style={{
+            textDecoration: task.completed ? "line-through" : "none",
+            textDecorationColor: task.completed ? "#eee" : "inherit",
+          }}
+        >
+          <input
+            type="checkbox"
+            checked={task.completed}
+            onChange={() => onCheck(task.id)}
+            className="check__box"
+          />
+          {task.task}
+        </div>
         <div>
-          <FontAwesomeIcon icon={faPenSquare} 
-          onClick={() => editTodo(task.id)} />
-          <FontAwesomeIcon icon={faTrash} className="fa__trash" 
-          onClick={() => deleteTodo(task.id)} />
+          <button
+            className="fa__edit"
+            title="edit task"
+            onClick={() => editTodo(task.id)}
+            aria-label="edit task"
+          >
+            <FontAwesomeIcon icon={faEdit} />
+          </button>
+          <button
+            className="fa__trash"
+            onClick={() => deleteTodo(task.id)}
+            title="delete task"
+            aria-label="delete task"
+          >
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
         </div>
       </div>
     </>
