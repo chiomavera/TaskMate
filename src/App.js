9d7549e68b27bb37b87f01075b1315cc71ec0755
chiomavera <you@example.com>
2023-03-15 20:18:26 +0100

    [FEAT]: add footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import TodoWrapper from './components/TodoWrapper';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div className="App">
      <TodoWrapper/>
+     <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p className='footer-text'>made with ❤ by <a className='github' href="https://github.com/chiomavera/">Vera</a></p>
+      <div>
+      <a  className='socials' href="https://twitter.com/nkanmuo_vera">twitter</a>
+      <a  className='socials' href="https://www.linkedin.com/in/chioma-vera-nkanmuo/">linkedIn</a>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
